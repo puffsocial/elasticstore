b8b99fb5cacdb266429e9b4c49f1fcd9905e3d2a
videah <videah@selfish.systems>
2021-07-29 04:13:56 +0100

    set references to index dispensaries

--- src/references.ts
+++ src/references.ts
@@ -3,9 +3,8 @@ import { Reference } from "./types"
 // Records should be added here to be indexed / made searchable
 const references: Array<Reference> = [
   {
-    collection: "example",
-    index: "example",
-    include: ["name", "email", "biography", "updatedAt", "createdAt"],
+    collection: "dispensary",
+    index: "dispensary",
   },
 ]
 
